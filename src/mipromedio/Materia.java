f317d547b69fb85bdf712f3d509b740d17b0f67b
Victor Ramirez <vramirezgarcia64@correo.unicordoba.edu.co>
2023-02-04 12:29:44 -0500

    Se agrega clase materia

---
+++ src/mipromedio/Materia.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mipromedio;
+
+/**
+ *
+ * @author pc
+ */
+public class Materia {
+    private String nombre;
+    private float nota;
+    private int creditos;
+
+    public Materia(String nombre, float nota, int creditos) {
+        this.nombre = nombre;
+        this.nota = nota;
+        this.creditos = creditos;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the nota
+     */
+    public float getNota() {
+        return nota;
+    }
+
+    /**
+     * @param nota the nota to set
+     */
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    /**
+     * @return the creditos
+     */
+    public int getCreditos() {
+        return creditos;
+    }
+
+    /**
+     * @param creditos the creditos to set
+     */
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }  
+}
