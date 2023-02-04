468283127c8a400cfc536182b7d862970f242211
Victor Ramirez <vramirezgarcia64@correo.unicordoba.edu.co>
2023-02-04 12:27:46 -0500

    Se agrega la base del proyecto

---
+++ src/mipromedio/MiPromedio.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mipromedio;
+
+/**
+ *
+ * @author pc
+ */
+public class MiPromedio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Vista vista = new Vista();
+        vista.setVisible(true);
+    }
+    
+}
